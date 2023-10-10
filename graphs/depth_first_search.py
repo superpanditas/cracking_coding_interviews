290ec57f015cc0133a2ae255d75225604b610359
Vaziago <panditas.datascientist@gmail.com>
2023-10-09 23:31:20 -0600

    add depth first search file

--- graphs/depth_first_search.py
+++ graphs/depth_first_search.py
@@ -1,12 +1,13 @@
 
 class Node:
+    # Initialize Class
     def __init__(self, name):
         self.children = []
         self.name = name
-
+    # Add Children 
     def addChildre(self, name):
         self.children.append(Node(name))
-
+        
     def depthFirstSearch(self, array):
         array.append(self.name)
         for child in self.children:
